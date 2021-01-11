1f6956b28390f1f438bb235763c59f85b5dd1469
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-01-11 20:44:19 +0100

    Completed Quote Generator

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
-
+const quoteButton = document.getElementById("new-quote");
+const container = document.getElementById("container");
+const load = document.getElementById("loader");
+const twitterBtn = document.getElementById("twitter");
 
 //Get Quote
 
 async function getQuote(){
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    loader()
+    const proxyUrl = "https://whispering-tor-04671.herokuapp.com/"
     const apiurl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try{
         const response = await fetch(proxyUrl + apiurl);
         const data = await response.json();
-        // quote.innerText = data.quoteText;
-        // author.innerText = data.quoteAuthor;
+        quote.innerText = data.quoteText;
+        if(data.quoteAuthor == ""){
+            author.innerText = "Unknown"
+        }else{
+            author.innerText = data.quoteAuthor;
+        }
         console.log(data)
     }catch(error){
         getQuote();
-        console.log("There is an Error", error)
     }
 }
 
+//put off display on funtion call
+function loader(){
+    container.style.display = "none";
+        load.style.display = "block";
+        //load and display after 2 seconds
+    setTimeout(function(){
+        container.style.display = "block";
+        load.style.display = "none";
+    },2500)
+}
 
-//populate UI
 
+//TweetQuote
 
+function TweetQuote(){
+    const QuoteText =  quote.innerText ;
+    const QuoteAuthor = author.innerText;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${QuoteText}-${QuoteAuthor}`;
+    window.open(tweetUrl, "_blank")
+}
 
+//Event Listeners
+quoteButton.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click',TweetQuote)
 
-//Call quote
+//on load
 getQuote()
